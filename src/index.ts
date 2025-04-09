580d5096db872adb6e70a902b17ceab13acc772b
Harshit Upadhyay <harshitu149@gmail.com>
2025-04-10 01:17:53 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken";
+
+
+const app = express ()
+
+
+// declaration file = file provide types to typeless libraries such as express 
+ 
+
+app.post("/api/v1/signin",( req, res)=>{
+
+})
+
+app.post("/api/v1/content",( req, res)=>{
+
+})
+
+app.get("/api/v1/content",( req, res)=>{
+
+})
+
+app.delete("/api/v1/content",( req, res)=>{
+
+})
+
+app.post("/api/v1/brain/share",( req, res)=>{
+
+})
+
+app.get ("api/v1/brain/: shareLink" , (req,res){
+
+})
+
+app.listen (3000);
+
+
